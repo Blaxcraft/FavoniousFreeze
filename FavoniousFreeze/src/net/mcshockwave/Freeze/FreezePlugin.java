eef4716ed811e95963b5523b8f74b5f347e2eb19
Blaxcraft <golfboy2300@yahoo.com>
2014-06-07 11:24:28 -0700

    Added code

---
+++ FavoniousFreeze/src/net/mcshockwave/Freeze/FreezePlugin.java
@@ -0,0 +1,36 @@
+package net.mcshockwave.Freeze;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FreezePlugin extends JavaPlugin implements Listener {
+	public static boolean	freezeEnabled	= false;
+
+	public void onEnable() {
+		Bukkit.getPluginManager().registerEvents(this, this);
+	}
+
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if (sender.isOp()) {
+			freezeEnabled = !freezeEnabled;
+
+			sender.sendMessage("§7Freeze §6" + (freezeEnabled ? "enabled" : "disabled"));
+		}
+		return true;
+	}
+
+	@EventHandler
+	public void onPlayerMove(PlayerMoveEvent event) {
+		Location f = event.getFrom();
+		Location t = event.getTo();
+		if ((freezeEnabled) && ((f.getX() != t.getX()) || (f.getY() != t.getY()) || (f.getZ() != t.getZ()))) {
+			event.setTo(f);
+		}
+	}
+}
